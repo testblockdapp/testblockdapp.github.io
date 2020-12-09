e3c8035a01f291e29932cb9634208ff762032398
tester <testdappdev@protonmail.com>
2020-12-08 22:41:13 -0600

    go

---
+++ js/auction.js
@@ -0,0 +1,408 @@
+// state = 1: == open to enter || state = 2: ready to collect || state = 3: == ended
+let lobbies = []
+
+function run_Auction() {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+		getTodayLobbyMobile()
+		getPastLobbiesMobile()
+	}else{
+		getTodayLobby()
+		getPastLobbies()
+	}
+}
+
+setInterval(() => {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+		getTodayLobbyMobile()
+	}else{
+		getTodayLobby()
+	}
+
+}, 1000 * 20)
+
+var mobileAuctionRendered = 0
+setInterval(() => {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+		if(	mobileAuctionRendered == 0){
+			mobileAuctionAdjuster()
+			getTodayLobbyMobile()
+			getPastLobbiesMobile()
+		}
+}, 1000)
+
+function getTodayLobby() {
+	if(currentDay < 1)
+		return
+    $('.fi-1')[0].innerHTML = "#" + currentDay
+
+    $('.fi-2')[0].innerHTML = abbreviate_number(calcDaysLobbyPayout(currentDay) / DESI, 2)
+
+    let daysTotalUsersEntry = 0
+    mainContract.methods.xfLobby(currentDay).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        daysTotalUsersEntry = parseFloat(res) / 10e9
+        $(`.fi-4`)[0].innerHTML = ((calcDaysLobbyPayout(currentDay)) / (daysTotalUsersEntry)).toFixed(2)
+		if( ( (daysTotalUsersEntry / 1e8) ) < 1)
+			$(`.fi-4`)[0].innerHTML = calcDaysLobbyPayout(currentDay) / 1e8                          
+    })
+
+    let totalUserEntryETH = 0
+    // how many times entered current day's lobby
+    mainContract.methods.xfLobbyMembers(currentDay, user.address).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        for (var i = 0; i < res.tailIndex; i++) {
+            mainContract.methods.xfLobbyEntry(user.address, currentDay, i).call({
+                shouldPollResponse: true,
+            }).then(res => {
+                totalUserEntryETH += parseFloat(res.rawAmount)
+                $('.fi-8')[0].innerHTML = abbreviate_number(totalUserEntryETH / 10e17, 7)
+                $('.fi-6')[0].innerHTML = abbreviate_number(((calcDaysLobbyPayout(currentDay) / DESI) * totalUserEntryETH / (daysTotalUsersEntry)), 2)
+
+
+                let daysTotalUsersEntry2 = 0
+                mainContract.xfLobby(currentDay).call({
+                    shouldPollResponse: true,
+                }).then(res => {
+                    daysTotalUsersEntry2 += parseFloat(tronWeb.fromSun(res))
+                    $('.fi-7')[0].innerHTML = abbreviate_number(((daysTotalUsersEntry2 * 0.95) * totalUserEntryETH / daysTotalUsersEntry), 2)
+                })
+
+            })
+        }
+    })
+
+    mainContract.methods.xfLobby(currentDay).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        daysTotalUsersEntry = parseFloat(res)
+        $(`.fi-10`)[0].innerHTML = abbreviate_number(daysTotalUsersEntry / 10e17, 7)
+    })
+
+}
+
+function calcDaysLobbyPayout(day) {
+    if (day <= 365) {
+        return 25000 * (100000000)
+    } else {
+        return 25000 * (100000000)
+    }
+}
+
+let clcD1 = true
+
+function getPastLobbies() {
+    $('.holder-list')[0].innerHTML = ""
+    for (var i = currentDay; i > 1; i--) {
+
+        let enBtn =
+            `
+        <div style="">
+            <button class="button w-24 shadow-md mr-1 mb-2 bg-gray-200 text-gray-600"
+                style="margin: 0;padding: 5px 10px;width: 85%; cursor: auto; float: right; background: #111 !important; color: #07d562 !important;">ENDED</button>
+        </div>
+        `
+
+        let activeRow = ""
+        if (!clcD1) activeRow = "tb-active-row-2"
+
+
+        $('.holder-list')[0].innerHTML +=
+            `
+        <div class="box px-4 py-4 mb-3 flex items-center zoom-in ${activeRow}"
+            style="cursor: auto; margin-bottom: 7px; color: #2eff3c; background: #A3A9AA;">
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 5%;text-align:center;font-weight: 900; color: #051242;">
+                <span class="fi-1-day-${i - 1} inbox__item--highlight">#${i - 1}</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block; width: 14%; text-align:center; font-weight: 900; color: #051242;">
+                <span class=fi-2-day-${i - 1} inbox__item--highlight">${abbreviate_number(calcDaysLobbyPayout(i - 1) / DESI, 2)}</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 15.0%;text-align:center;font-weight: 900; color: #051242;">
+                <span class="fi-4-day-${i - 1} inbox__item--highlight">--</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 16.0%;text-align:center;font-weight: 900; color: #051242;">
+                <span class=fi-5-day-${i - 1} inbox__item--highlight">--</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 15.5%;text-align:center;font-weight: 900; color: #051242;">
+                <span class="fi-6-day-${i - 1} inbox__item--highlight" style="padding-right: 0%;">--</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 15.5%;text-align:center;font-weight: 900; color: #051242;">
+                <span class="fi-8-day-${i - 1} inbox__item--highlight">--</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 15.5%;text-align:center;font-weight: 900; color: #051242;">
+                <span class="fi-10-day-${i - 1} inbox__item--highlight">--</span>
+            </div>
+
+            <div class="fi-9-day-${i - 1}" style="display:inline-block;width: 15.5%;">${enBtn}</div>
+        </div>
+        `
+
+        midlleCaller(i - 1)
+
+        clcD1 = !clcD1
+    }
+}
+
+let clcA = 1
+function midlleCaller(i) {
+    clcA ++
+
+    setTimeout(() => {
+        getLobbyData(i)
+    }, 300 * clcA)
+}
+function getLobbyData(day) {
+    let daysTotalUsersEntry = 0
+    mainContract.methods.xfLobby(day).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        console.log((calcDaysLobbyPayout(day) ) / daysTotalUsersEntry)
+        
+        daysTotalUsersEntry += parseFloat(res) / 10e9
+        $(`.fi-4-day-${day}`)[0].innerHTML = ((calcDaysLobbyPayout(day) ) / (daysTotalUsersEntry )).toFixed(2)
+		if( ( (daysTotalUsersEntry / 1e8) ) < 1)
+			$(`.fi-4-day-${day}`)[0].innerHTML = calcDaysLobbyPayout(day) / 1e8
+
+        $(`.fi-5-day-${day}`)[0].innerHTML = "closed"
+        $(`.fi-10-day-${day}`)[0].innerHTML = abbreviate_number(daysTotalUsersEntry / 10e7, 7)
+    })
+
+    let totalUserEntryETH = 0
+    // how many times entered current day's lobby
+    mainContract.methods.xfLobbyMembers(day, user.address).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        for (var i = 0; i < res.tailIndex; i++) {
+            mainContract.methods.xfLobbyEntry(user.address, day, i).call({
+                shouldPollResponse: true,
+            }).then(res => {
+                totalUserEntryETH += parseFloat(res.rawAmount)
+                $(`.fi-8-day-${day}`)[0].innerHTML = abbreviate_number(totalUserEntryETH / 10e17, 7)
+                $(`.fi-6-day-${day}`)[0].innerHTML = abbreviate_number(((calcDaysLobbyPayout(day) / 1e11) * totalUserEntryETH / (daysTotalUsersEntry * 10e6)), 2)
+
+
+                let daysTotalUsersEntry2 = 0
+                mainContract.methods.xfLobby(day).call({
+                    shouldPollResponse: true,
+                }).then(res => {
+                    daysTotalUsersEntry2 += parseFloat(res)
+
+                })
+
+                $(`.fi-9-day-${day}`)[0].innerHTML =
+                    `
+                <div style="">
+                    <button class="button w-24 shadow-md mr-1 mb-2 bg-theme-9 text-white"
+                        style="margin: 0;padding: 5px 10px;float: right;width: 85%; float: right;" onClick="collectLobby(${day})">COLLECT</button>
+                </div>
+                `
+            })
+        }
+
+
+    })
+
+}
+
+function collectLobby(day) {
+    mainContract.methods.xfLobbyExit(day, 0).send({
+        from: user.address,
+        shouldPollResponse: true
+    }).then(res => {}).catch(err => {
+        console.error(err, "er")
+    }).finally(res => {
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+			getPastLobbiesMobile()
+		}else 
+			getPastLobbies()
+    })
+}
+
+function enterLobby() {
+	if( currentDay < 1 ){
+		$('.lobby-button')[0].innerHTML = ""
+		$('.lobby-button')[1].innerHTML = ""
+		alert("Auction not opened yet!")
+		return
+	}
+	
+    $('.btn-auction-enter-load')[0].style.display = "block"
+    $('.btn-auction-enter-txt')[0].innerHTML = ""
+
+    let int1 = setInterval(() => {
+        if (!$(".modal.show")[0]) {
+            clearInterval(int1)
+            $('.btn-auction-enter-load')[0].style.display = "none"
+            $('.btn-auction-enter-txt')[0].innerHTML = "ENTER"
+        }
+    }, 300)
+}
+
+function enterLobbyFinal() {
+    let referrer = user.referrer
+    if (user.referrer === zeroAddress) referrer = "0xfB4436f83Fd9102b93E6B4d6a6a437424E4accC0"
+ 
+    mainContract.methods.xfLobbyEnter(referrer).send({
+        from: user.address,
+        shouldPollResponse: true, 
+        value: parseInt(parseFloat($('.auction-amount-entry')[0].value) * 10e17)
+    }).then(res => {
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+			getTodayLobbyMobile()
+		}else 
+			getTodayLobby()
+    }).catch(err => {
+        console.error(err, "er")
+    }).finally(res => {})
+
+    $('.close-modal-a').click()
+}
+
+
+//MOBILE VIEWING CONVERSIONS
+function mobileAuctionAdjuster(){
+	
+	//Mobile Header Adjustments
+	for(let i = 0; i < $('.mobile-auction-hide').length; i++){
+		$('.mobile-auction-hide')[i].innerHTML = ""
+	}
+	
+	let headers = "Day &nbsp&nbsp&nbsp&nbsp Available CSE &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  Receive &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Your Entry &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Total Entries &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Status "
+	$('.auction-headers')[0].innerHTML = headers
+	$('.auction-headers')[0].style.fontSize = "8px"
+	$('.auction-headers')[0].style.width = "100vw"
+	$('.auction-headers')[0].style.marginLeft = "0px"
+	$('.auction-headers')[0].style.textAlign = "left"
+	$('.mobile-auction-resize')[0].style.marginLeft = "0px"
+	$('.mobile-auction-resize')[0].style.textAlign = "left"
+	$('.mobile-auction-resize')[0].style.width = "100vw"
+
+    $('.fi-1')[0].innerHTML = "#" + currentDay //Day
+    $('.fi-1')[0].style.width = "5vw"
+    $('.fi-2')[0].innerHTML = abbreviate_number(calcDaysLobbyPayout(currentDay) / DESI, 2) //Total NUG Available
+    $('.fi-2')[0].style.width = "16vw"
+	
+	//negate useless mobile info for current day
+	$('.fi-4-div')[0].innerHTML = ""
+	$('.fi-5-div')[0].innerHTML = ""
+	$('.fi-4-div')[0].style.width = "0vw"
+	$('.fi-5-div')[0].style.width  = "0vw"
+	
+	//set mobile widths and px sizes for current day
+	$('.fi-1-div')[0].style.width = "5vw"
+	$('.fi-1')[0].style.textAlign = "center"
+	
+	$('.fi-2-div')[0].style.width = "16vw"
+	$('.fi-2')[0].style.textAlign = "center"
+
+	$('.fi-6-div')[0].style.width = "16vw"
+	$('.fi-6')[0].style.textAlign = "center"
+
+	$('.fi-8-div')[0].style.width = "16vw"
+	$('.fi-8')[0].style.textAlign = "center"
+
+	$('.fi-10-div')[0].style.width = "16vw"	
+	$('.fi-10')[0].style.textAlign = "center"
+	
+	mobileAuctionRendered = 1
+
+}
+
+function getTodayLobbyMobile() {
+	mobileAuctionAdjuster()
+	
+    let daysTotalUsersEntry = 0
+    mainContract.methods.xfLobby(currentDay).call({
+        shouldPollResponse: true,
+    }).then(res => {
+        daysTotalUsersEntry = parseFloat(res)
+        $(`.fi-10`)[0].innerHTML = abbreviate_number(daysTotalUsersEntry / 10e17, 7)
+    })
+	
+}
+
+function getPastLobbiesMobile() {
+    $('.holder-list')[0].innerHTML = ""
+    for (var i = currentDay; i > 1; i--) {
+        let enBtn =
+            `
+        <div style="">
+            <button class="button w-24 shadow-md mr-1 mb-2 bg-gray-200 text-gray-600"
+                style="margin: 0;padding: 2px 3px;width: 20vw; cursor: auto; float: center; ">ENDED</button>
+        </div>
+        `
+
+        let activeRow = ""
+        if (!clcD1) activeRow = "tb-active-row-2"
+
+
+        $('.holder-list')[0].innerHTML +=
+            `
+        <div class="box px-4 py-4 mb-3 flex items-center zoom-in ${activeRow}"
+            style="cursor: auto; margin-bottom: 7px;color: #051242; background: #aa8c2c;">
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 5vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-1-day-${i - 1} inbox__item--highlight">#${i - 1}</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block; width: 16vw; text-align:center; font-weight: 900; color: #051242;">
+                <span class="fi-2-day-${i - 1} inbox__item--highlight">${abbreviate_number(calcDaysLobbyPayout(i - 1) / DESI, 2)}</span>
+            </div>
+
+            <div class="fi-4-div-${i - 1} w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 0vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-4-day-${i - 1} inbox__item--highlight">..</span>
+            </div>
+
+            <div class="fi-5-div-${i - 1} w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 0vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-5-day-${i - 1} inbox__item--highlight">..</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 16vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-6-day-${i - 1} inbox__item--highlight"">..</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 16vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-8-day-${i - 1} inbox__item--highlight"">..</span>
+            </div>
+
+            <div class="w-64 sm:w-auto truncate"
+                style="display:inline-block;width: 16vw;text-align:center;font-weight: 900;color: #051242;">
+                <span class="fi-10-day-${i - 1} inbox__item--highlight"">..</span>
+            </div>
+
+            <div class="fi-9-day-${i - 1}" style="display:inline-block;width: 16vw;">${enBtn}</div>
+        </div>
+        `
+		
+        midlleCaller(i - 1)
+
+        clcD1 = !clcD1
+    }
+    for (var i = 0; i < currentDay; i++) {
+		$('.fi-4-div-${i}')[0].innerHTML = ""
+		$('.fi-5-div-${i}')[0].innerHTML = ""
+		$('.fi-4-div-${i}')[0].style.width = "0vw"
+		$('.fi-5-div-${i}')[0].style.width = "0vw"
+	}
+}
---
+++ shared/header.js
@@ -0,0 +1,130 @@
+$('.shared-header')[0].innerHTML =
+`
+<div class="mobile-menu md:hidden" style="border: 0;">
+    <div class="mobile-menu-bar" style="background: #eff3f6; box-shadow: 0px 0px 10px #07d562; height: 60px;">
+        <a class="flex mr-auto" onclick="openMenuM()">
+            <img alt="CSNE token" class="w-6" src="./img/logo.png" style="width: 10vw;filter: drop-shadow(0px 0px 3px #07d562);">
+        </a>
+
+        <div class="top-bar-boxed flex items-center">
+            <div style="background: #eff3f6;/* box-shadow: 2px 2px 8px #07d562; */height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #2eff3c;font-size: 18px;font-weight: 500;margin-right: 20px;"><span class="day-end-in" id="countdownm" style="padding-right: 10px; color: #111 !important;">Auction ends at 00:00 UTC</span></div>
+
+            <div style="background: #eff3f6;box-shadow: 2px 2px 8px #07d562;height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;margin-right: 10px;"><span class="my-acc-add"></span><img class="img-mm" src="https://cdn.discordapp.com/attachments/572416318234427402/771703875602350090/metamask-2728406-2261817.png" style="width: 28px;margin-top: -3.7px;margin: 0px 1px 0px 10px;float: right;border-radius: 4px;"></div>
+        </div>
+
+        <a onclick="openMenuM()"> <i data-feather="bar-chart-2"
+                class="w-8 h-8 text-white transform -rotate-90" style="color: #111 !important;"></i> </a>
+    </div>
+
+    <div class="mobile-exten" style="background: #eff3f6;box-shadow: 0px 0px 10px #2a230b;height: 60px;z-index: 1000000000;position: absolute;width: 100%;display: flex;align-items: center;display: none; height: auto;">
+
+        <ul class="border-t border-theme-24 py-7" style="color: red;">
+             <li>
+                <a href="stake.html" class="menu">
+                    <div class="menu__icon"> <i data-feather="layers"></i> </div>
+                    <div class="menu__title"> Stake </div>
+                </a>
+            </li>
+            <li>
+                <a href="auction.html" class="menu">
+                    <div class="menu__icon"> <i data-feather="server"></i> </div>
+                    <div class="menu__title"> Auction </div>
+                </a>
+            </li>
+            <li>
+				<span class="telemenu">
+                <a href="" class="menu ">
+                    <div class="menu__icon"> <i data-feather="users"></i> </div>
+                    <div class="menu__title"> Telegram </div>
+                </a>
+				</span>
+            </li>    
+            <li>
+				<span class="trcmenu">
+					<a href="trcwebsite" class="menu ">
+                    <div class="menu__icon"> <i data-feather="server"></i> </div>
+                    <div class="menu__title"> NAME </div>
+					</a>
+				</span>
+            </li> 
+            <li>
+                <a href="refer.html" class="menu ">
+                    <div class="menu__icon"> <i data-feather="users"></i> </div>
+                    <div class="menu__title"> Refer & FAQ </div>
+                </a>
+            </li>   			
+        </ul>
+    </div>
+
+
+</div>
+<div class="border-b hd-nm border-theme-24 -mt-10 md:-mt-5 -mx-3 sm:-mx-8 px-3 sm:px-8 pt-3 md:pt-0 mb-10" style="background: #eff3f6;border-bottom: 1px solid #040417;">
+    <div class="top-bar-boxed flex items-center">
+        <!-- BEGIN: Logo -->
+        <div class="-intro-x hidden md:flex">
+            <img src="./img/logo.png" style="width: 6rem; margin-top: 1rem;">
+            <span class="text-white text-lg ml-3" style="padding-top: 0.2rem;font-size: 28px;padding-left: 5px;font-weight: 500;color: #ecf590;"></span>
+        </div>
+        <!-- END: Logo -->
+        <!-- BEGIN: Breadcrumb -->
+        <div class="-intro-x breadcrumb breadcrumb--light mr-auto" style="background: #eff3f6; border-left: 0px solid #040417;"> 
+        <span style="font-size: 20px;color: #111;font-weight: 400;">CSE ETH</span></div>
+        <!-- END: Breadcrumb -->
+        <div style="background: #eff3f6;/* box-shadow: 2px 2px 8px #07d562; */height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;margin-right: 20px;"><span class="trcwebsite" style="padding-right: 10px;"><a href="" target="_blank">TRC Website</a></span></div>
+
+        <div style="background: #eff3f6;/* box-shadow: 2px 2px 8px #07d562; */height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;margin-right: 20px;"><span class="telegram" style="padding-right: 10px;"><a href="https://t.me" target="_blank">Telegram</a></span></div>
+
+        <div style="background: #eff3f6;/* box-shadow: 2px 2px 8px #07d562; */height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;margin-right: 20px;"><span class="etherscan" style="padding-right: 10px;"><a href="https://etherscan.io/" target="_blank">Etherscan</a></span></div>
+
+        <div style="background: #eff3f6;/* box-shadow: 2px 2px 8px #07d562; */height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;margin-right: 20px;"><span class="day-end-in" id="countdown" style="padding-right: 10px;">Auction ends at: 00:00:00</span></div>
+
+        <div style="background: #eff3f6;box-shadow: 2px 2px 8px #07d562;height: 38px;border-radius: 6px;padding: 2px 0px 0px 10px;border: 3px solid #07d562;color: #111;font-size: 18px;font-weight: 500;"><span class="my-acc-add">...</span><img class="img-metamask" src="https://cdn.discordapp.com/attachments/572416318234427402/771703875602350090/metamask-2728406-2261817.png" style="width: 28px;margin-top: -3.7px;margin: 0px 1px 0px 10px;float: right;border-radius: 4px;"></div>
+    </div>
+</div>
+`
+
+let mmO
+setInterval(() => {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('.hd-nm')[0].style.display = "none"
+        $('.my-acc-add')[0].style.fontSize = "8px"
+        $('.day-end-in')[0].style.fontSize = "8px"
+	}
+}, 1000)
+
+function openMenuM() {
+    if (mmO) {
+        mmO = false
+        $('.mobile-exten')[0].style.display = "none"
+    } else {
+        mmO = true
+        $('.mobile-exten')[0].style.display = "block"
+    }
+}
+
+    function zeroPrefix(val) {
+        if ((val.toString()).length < 2) {
+            val = '0' + val;
+        }
+
+        return val;
+    }	
+
+    setInterval(function time() {
+        let d = new Date(),
+            hours = 23 - d.getUTCHours(),
+            min = 59 - d.getUTCMinutes(),
+            sec = 59 - d.getUTCSeconds();
+		if(currentDay < 1){
+			document.getElementById('countdown').innerHTML = 'Auction Starts: ' + zeroPrefix(hours) + ' : ' + zeroPrefix(min) + ' : ' + zeroPrefix(sec);
+			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+				document.getElementById('countdownm').innerHTML = 'Auction Starts: ' + zeroPrefix(hours) + ' : ' + zeroPrefix(min) + ' : ' + zeroPrefix(sec);
+			}
+		}else{
+			document.getElementById('countdown').innerHTML = 'Auction End: ' + zeroPrefix(hours) + ' : ' + zeroPrefix(min) + ' : ' + zeroPrefix(sec);
+			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+				document.getElementById('countdownm').innerHTML = 'Auction End: ' + zeroPrefix(hours) + ' : ' + zeroPrefix(min) + ' : ' + zeroPrefix(sec);
+			}
+		}
+		
+    }, 1000);
