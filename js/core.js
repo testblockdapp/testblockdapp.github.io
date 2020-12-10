9a6a7ddb9c57801fb34d1a0679f2cde8f74da12c
tester <testdappdev@protonmail.com>
2020-12-10 01:29:47 -0600

    Update core.js

--- js/core.js
+++ js/core.js
@@ -20,7 +20,7 @@ $('.trcwebsite')[0].innerHTML = '<a href="'+trcwebsite+'" target="_blank">CSE-Tr
 $('.trcmenu')[0].innerHTML = '<a href="'+trcwebsite+'" target="_blank" class="menu"><div class="menu__icon"><i data-feather="server"></i> </div><div class="menu__title"> CSE Tron </div></a>'
 $('.telemenu')[0].innerHTML = '<a href="'+telegram+'" target="_blank" class="menu"><div class="menu__icon"><i data-feather="users"></i> </div><div class="menu__title"> Telegram </div></a>'
 
-var csnLogo = '<div class="flex items-center justify-center""><img src="./img/logo.png" style="display: inline-block;width: 300px; height: 175px;"></div>';
+var csnLogo = '<div class="flex items-center justify-center""><img src="./img/logo.png" style="display: inline-block;width: 300px; height: 200px;"></div>';
 var socialMediaSites = '<div class="flex items-center justify-center" style="border: 0;padding: 12px;">' +
 '<div class="contact-info social-media"><a href="https://t.me/csetron"target="blank" class="fa fa-telegram" style="font-size:25px; color: #07d562; margin-right: 0px;"></a></div>' 
  +
