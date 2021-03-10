904850aa4acbe19e306ab50dfceff843f7d1617a
shonuff1221 <shonuff4521@gmail.com>
2021-03-10 07:16:41 -0500

    Update stake.js

--- js/stake.js
+++ js/stake.js
@@ -529,7 +529,7 @@ function renderMyEndedStakes(lockedDay, servedDays, stakedSuns, dividends, payou
 
                     <div class="w-64 sm:w-auto truncate ended-stake-info-2"
                         style="width: 50vw; text-align:center; font-weight: 900; color: #85B6FF;">
-                        <span class="inbox__item--highlight">${stakedDays}</span>
+                        <span class="inbox__item--highlight">${"#stakedDays"}</span>
                     </div>
 
                     <div class="w-64 sm:w-auto truncate ended-stake-info-3"
